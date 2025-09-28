c58d9453d804328a07392fbdc61dee544c88715d
devanescent <devanescent@gmail.com>
2025-09-28 22:21:58 +0200

    Added day 01 part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -5,10 +5,33 @@ pub fn solve_part1(input: &String) -> AoCResult {
     let res: u64 = input
         .lines()
         .map(|l| u64::from_str(l).unwrap())
-        .map(|n| n / 3 - 2)
+        .map(get_fuel)
         .sum();
 
     AoCResult::Num(res)
+}
+
+pub fn solve_part2(input: &String) -> AoCResult {
+    let res: u64 = input
+        .lines()
+        .map(|l| u64::from_str(l).unwrap())
+        .map(get_fuel_recursive)
+        .sum();
+
+    AoCResult::Num(res)
+}
+
+fn get_fuel(mass: u64) -> u64 {
+	u64::saturating_sub(mass / 3, 2)
+}
+
+fn get_fuel_recursive(mass: u64) -> u64 {
+	let fuel = get_fuel(mass);
+	if fuel > 0 {
+		fuel + get_fuel_recursive(fuel)
+	} else {
+		0
+	}
 }
 
 #[cfg(test)]
@@ -42,5 +65,27 @@ mod tests {
         let input = String::from("100756");
         let result = solve_part1(&input);
         assert_eq!(u64::from(result), 33583);
+    }
+
+    #[test]
+    fn part2_example1() {
+        let input = String::from("14");
+        let result = solve_part2(&input);
+        assert_eq!(u64::from(result), 2);
+    }
+
+    #[test]
+
+    fn part2_example2() {
+        let input = String::from("1969");
+        let result = solve_part2(&input);
+        assert_eq!(u64::from(result), 966);
+    }
+
+    #[test]
+    fn part2_example3() {
+        let input = String::from("100756");
+        let result = solve_part2(&input);
+        assert_eq!(u64::from(result), 50346);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,6 @@ use std::fs;
 fn main() {
     let data =
         fs::read_to_string("input.txt").expect("Expected file 'input.txt' in project directory");
-    println!("Day 01: {}", day01::solve_part1(&data));
+    println!("Day 01 (part 1): {}", day01::solve_part1(&data));
+    println!("Day 01 (part 2): {}", day01::solve_part2(&data));
 }
